cb6d54599ef1ce5f17197bb5ab468ec9b8edebf9
intasolutions <intasolutionpvtltd@gmail.com>
2026-01-21 21:40:45 +0530

    Configure STATIC_ROOT and MEDIA_ROOT

--- backend/revive_cms/settings.py
+++ backend/revive_cms/settings.py
@@ -137,6 +137,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Auth User Model
 AUTH_USER_MODEL = 'users.User'
